b91768ec79c9c317626f1feb83b1cb4dcef7c7de
Ben Hasselgren <ben.nadalito@gmail.com>
2018-04-13 16:58:49 +0000

    Finished boggle section 2

--- boggle.py
+++ boggle.py
@@ -1,38 +1,50 @@
+import os
 from string import ascii_uppercase
 from random import choice
 
-def  make_grid(width, height):
-    """
-    Create a grid that will hold all of the tiles for boggle game
-    """
-    return{(row, col): choice(ascii_uppercase) 
-        for row in range(height)
-        for col in range(width)
-    }
+SCRIPT_PATH = os.path.join(os.getcwd(), os.path.dirname(__file__))
 
+def make_grid(width, height):
+    """
+    Create a grid that will hold all of the tiles for a boggle game
+    """
+    return {(row, col): choice(ascii_uppercase) for row in range(height) for col in range(width)}
+    
 def neighbours_of_position(coords):
     """
-    Get neighbours of  given position
+    Get neighbours of a given position
     """
+    
     row = coords[0]
     col = coords[1]
     
-    top_left = (row - 1, col - 1)
-    top_center = (row -1, col)
-    top_right = (row -1, col + 1)
+    #Assign each of the neighbours
     
-    left = (row, col -1)
-    right = (row, col + 1)
+    #Top-left to top-right
+    top_left = (row-1, col-1)
+    top_center = (row-1, col)
+    top_right = (row-1, col+1)
     
+    #Left to right
+    left = (row,col-1)
+    # The `(row, col)` coordinates passed to this
+    # function are situated here
+    right = (row,col+1)
+
+    # Bottom-left to bottom-right
     bottom_left = (row + 1, col - 1)
-    bottom_center = (row +1, col)
-    bottom_right = (row +1, col + 1)
+    bottom_center = (row + 1, col)
+    bottom_right = (row + 1, col + 1)
     
-    return[top_left, top_center, top_right, left, right, bottom_left, bottom_center, bottom_right]
     
+    return [top_left, top_center, top_right,
+            left, right,
+            bottom_left, bottom_center, bottom_right]
+
 def all_grid_neighbours(grid):
     """
-    Get all possible neighbours in grid
+    Get all of the possible neighbours for each position in
+    the grid
     """
     neighbours = {}
     for position in grid:
@@ -41,38 +53,73 @@ def all_grid_neighbours(grid):
     return neighbours
     
 def path_to_word(grid, path):
+    """
+    Add all of the letters on the path to a string
+    """
     return ''.join([grid[p] for p in path])
-    
+
 def search(grid, dictionary):
+    """
+    Search through the paths to locate words by matching strings to words in a dictionary
+    """
     neighbours = all_grid_neighbours(grid)
     paths = []
+    full_words, stems = dictionary
     
     def do_search(path):
         word = path_to_word(grid, path)
-        if word in dictionary:
+        if word in full_words:
             paths.append(path)
+        if word not in stems:
+            return
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
                 do_search(path + [next_pos])
+    
     for position in grid:
         do_search([position])
-    
+        
     words = []
     for path in paths:
         words.append(path_to_word(grid, path))
     return set(words)
-    
-def get_dictionary(dictionary_file):
-    with open(dictionary_file) as f:
-        return [w.strip().upper() for w in f]
 
-def main():
-    grid = make_grid(3,3)
-    dictionary = get_dictionary('words.txt')
-    words = search(grid, dictionary)
+def get_dictionary(dictionary_file):
+    """
+    Load Dictionary file
+    """
+    full_words, stems = set(), set()
+    if not dictionary_file.startswith('/'):
+        # if not absolute, then make path relative to our location:
+        dictionary_file = os.path.join(SCRIPT_PATH, dictionary_file)
+    
+    with open(dictionary_file) as f:
+        for word in f:
+            word = word.strip().upper()
+            full_words.add(word)
+            
+            for i in range(1, len(word)):
+                stems.add(word[:i])
+    return full_words, stems
+
+def display_words(words):
     for word in words:
         print(word)
     print("Found %s words" % len(words))
     
+        
+def main():
+    """
+    This is the function that will run the whole project
+    """
+    grid = make_grid(3, 3)
+    """
+    Here you can change your grid from a 3x3 to a 2x2 to test run times
+    """
+    dictionary = get_dictionary("words.txt")
+    words = search(grid, dictionary)
+    display_words(words)
+
 if __name__ == "__main__":
+    # Code in here will only execution when the file is run directly    
     main()
